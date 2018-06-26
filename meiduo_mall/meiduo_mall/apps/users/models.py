81f3a94531910aedaab53daf64e7865acbb2eb72
lgl <lgl@163.com>
2018-06-26 09:15:51 +0800

    邮箱验证前后端实现

--- meiduo_mall/meiduo_mall/apps/users/models.py
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from itsdangerous import TimedJSONWebSignatureSerializer as TJWSSerializer
+from itsdangerous import TimedJSONWebSignatureSerializer as TJWSSerializer, BadData
 
 # Create your models here.
 from users import constants
@@ -23,7 +23,26 @@ class User(AbstractUser):
         生成验证邮箱的url
         """
         serializer = TJWSSerializer(settings.SECRET_KEY, expires_in=constants.VERIFY_EMAIL_TOKEN_EXPIRES)
-        data = {'user_id': self.id, 'email': self.email} #嵌入token的数据，可以自己更改，只要生成的url能够唯一标识用户是谁即可
-        token = serializer.dumps(data).decode()  # 因为发给每个人的链接都不一样，所以写在模型类中了，又因为怕别人篡改url中的user_id所以使用itsdangerous生成token，而把用户id嵌入token
+        # 嵌入token的数据，可以自己更改，只要生成的url能够唯一标识用户是谁即可
+        data = {'user_id': self.id, 'email': self.email}
+        token = serializer.dumps(data).decode()
+        # 因为发给每个人的链接都不一样，所以写在模型类中了，又因为怕别人篡改url中的user_id所以使用itsdangerous生成token，而把用户id嵌入token
         verify_url = 'http://www.meiduo.site:8080/success_verify_email.html?token=' + token
         return verify_url
+
+    @staticmethod
+    def check_verify_email_token(token):
+        serializer = TJWSSerializer(settings.SECRET_KEY,expires_in=constants.VERIFY_EMAIL_TOKEN_EXPIRES)
+        try:
+            data = serializer.loads(token)
+        except BadData:
+            return None
+        else:
+            user_id = data['user_id']
+            email = data['email']
+            try:
+                user = User.objects.get(id = user_id,email = email)
+            except User.DoesNotExist:
+                return None
+            else:
+                return user
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -99,6 +100,27 @@ class EmailView(UpdateAPIView):
     #     # 查询user
     #     # 更新数据
     #     # 序列化返回
+
+
+# url(r'^emails/verification/$', views.VerifyEmailView.as_view()),
+class VerifyEmailView(APIView):
+    """
+    邮箱验证
+    """
+    def get(self,request):
+        # 获取token
+        token = request.query_params.get('token')
+        if not token:
+            return Response({'message': '缺少token'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 验证token
+        user = User.check_verify_email_token(token)
+        if user is None:
+            return Response({'message': '链接信息无效'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            user.email_active = True
+            user.save()
+            return Response({'message': 'OK'})
 
 
 
