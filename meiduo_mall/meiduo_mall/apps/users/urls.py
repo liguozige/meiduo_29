bf820e2d4da56059fd3d3b2c49f17e10bfcaa9e3
lgl <lgl@163.com>
2018-06-26 10:13:00 +0800

    省市区数据库模型类创建和导入测试数据脚本

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -10,6 +10,6 @@ urlpatterns=[
     url(r'^authorizations/$', obtain_jwt_token),  # 登录认证
     url(r'^user/$', views.UserDetailView.as_view()),  # 个人中心基本信息
     url(r'^email/$',views.EmailView.as_view()),  # 保存邮箱，并发送邮件
-    url(r'^emails/verification/$', views.VerifyEmailView.as_view()),
+    url(r'^emails/verification/$', views.VerifyEmailView.as_view()),#验证邮箱
 ]
 
---
+++ meiduo_mall/scripts/import_area_data_to_db.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysql -uroot -pmysql meiduo_29 < ./areas.sql
