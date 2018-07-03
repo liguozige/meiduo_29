44e7fc415fa01980f6ff6ba3d07226ca921f3522
lgl <lgl@163.com>
2018-07-03 21:31:14 +0800

    保存订单后端实现

--- meiduo_mall/meiduo_mall/apps/orders/serializers.py
+++ meiduo_mall/meiduo_mall/apps/orders/serializers.py
@@ -1,10 +1,12 @@
 from _decimal import Decimal
-from datetime import timezone
+from django.utils import timezone
 
 from django.db import transaction
 from django_redis import get_redis_connection
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+
+
 
 from goods.models import SKU
 from orders.models import OrderInfo, OrderGoods
@@ -32,10 +34,11 @@ class SaveOrderSerializer(serializers.ModelSerializer):
     """
     保存订单数据的序列化器
     """
+    #一些基本的校验
     class Meta:
         model = OrderInfo
         fields = ('order_id', 'address', 'pay_method')  # 来自OrderInfo
-        read_only_fields = ('order_id',)  # 不是自增的，
+        read_only_fields = ('order_id',)  # 不再是自增的了，也没有只读的性质了
         extra_kwargs = {
             'address': {
                 'write_only': True,  # 不返回，只是用来检查的
@@ -45,131 +48,235 @@ class SaveOrderSerializer(serializers.ModelSerializer):
             }
         }
 
+    #保存订单
     def create(self, validated_data):
         """
         保存订单
         """
-        #获取用户对象 user
-        # user = self.context['request'].user
-
-        #查询购物车redis  sku_id count  selected
-
-        #查询商品数据库 获取商品数据（库存）
-
-        #保存订单
-
-        #创建订单基本信息表记录 OrderInfo
-
-        #遍历需要结算的商品数量
-            #判断库存
-            # 库存减少 销量增加
-
-            #创建订单商品信息表记录 OrderGoods
-
-        #删除购物车中已结算的商品
-
-        #返回OrderInfo对象
-
-
-        # 获取当前下单用户
-        user = self.context['request'].user
-
-        # 组织订单编号 20170903153611+user.id
-        # timezone.now() -> datetime
-        order_id = timezone.now().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
-
         address = validated_data['address']
         pay_method = validated_data['pay_method']
+        # 获取用户对象 user
+        user = self.context['request'].user
 
-        # 生成订单
+        # 查询购物车redis  sku_id count  selected
+        redis_conn = get_redis_connection('cart')
+        #hash 商品数量 sku_id  count
+        redis_cart_dict = redis_conn.hgetall('cart_%s'%user.id)
+        #set 勾选商品
+        redis_cart_selected = redis_conn.smembers('cart_selected_%s'%user.id)
+
+        cart = {}
+        for sku_id in redis_cart_selected:
+            cart[int(sku_id)] = int(redis_cart_dict[sku_id])
+
+        if not cart:
+            raise serializers.ValidationError('购物车没有可结算的商品')
+
+
+        #创建事务 开启一个事务
         with transaction.atomic():
-            # 创建一个保存点
-            save_id = transaction.savepoint()
-
             try:
-                # 创建订单信息
+                save_id = transaction.savepoint()
+
+                # 保存订单
+                #生成订单编号order_id
+                #20180703201905 用户id
+                #datetime --> str   strftime
+                #str --> datetime   strptime
+                order_id = timezone.now().strftime('%Y%m%d%H%M%S')+ ('%09d' % user.id)
+
+                # 创建订单基本信息表记录 OrderInfo
                 order = OrderInfo.objects.create(
                     order_id=order_id,
-                    user=user,
-                    address=address,
-                    total_count=0,  # 每个商品的总数量
-                    total_amount=Decimal(0),  # 总金额
-                    freight=Decimal(10),  # 运费
-                    pay_method=pay_method,
-                    status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'] if pay_method == OrderInfo.PAY_METHODS_ENUM[
-                        'CASH'] else OrderInfo.ORDER_STATUS_ENUM['UNPAID']
+                    user = user,
+                    address = address,
+                    total_count = 0,
+                    total_amount = Decimal('0'),
+                    freight = Decimal('10.00'),
+                    pay_method = pay_method,
+                    status = OrderInfo.ORDER_STATUS_ENUM['UNSEND'] if pay_method == OrderInfo.PAY_METHODS_ENUM['CASH'] else OrderInfo.ORDER_STATUS_ENUM['UNPAID']
+
                 )
-                # 获取购物车信息
-                redis_conn = get_redis_connection("cart")
-                redis_cart = redis_conn.hgetall("cart_%s" % user.id)
-                cart_selected = redis_conn.smembers('cart_selected_%s' % user.id)
+                # 查询商品数据库 获取商品数据（库存） [1,2,3]
+                sku_id_list = cart.keys()
+                sku_obj_list = SKU.objects.filter(id__in=sku_id_list)
 
-                # 将bytes类型转换为int类型
-                cart = {}
-                for sku_id in cart_selected:
-                    cart[int(sku_id)] = int(redis_cart[sku_id])
-
-                # 一次查询出所有商品数据
-                skus = SKU.objects.filter(id__in=cart.keys())
-
-                # 处理订单商品
-                for sku in skus:
+                # 遍历需要结算的商品数量
+                for sku in sku_obj_list:
+                    #用户需要购买的数量
                     sku_count = cart[sku.id]
-
-                    # 判断库存
-                    origin_stock = sku.stock  # 原始库存
-                    origin_sales = sku.sales  # 原始销量
-
-                    if sku_count > origin_stock:
-                        # 库存不足，就回滚到保存点
+                    #判断库存
+                    if sku.stock<sku_count:
+                        #回滚到保存点
                         transaction.savepoint_rollback(save_id)
-                        raise serializers.ValidationError('商品库存不足')
+                        raise serializers.ValidationError('商品%s库存不足'%sku.name)
 
-                    # 用于演示并发下单
-                    # import time
-                    # time.sleep(5)
-
-                    # 减少库存
-                    new_stock = origin_stock - sku_count
-                    new_sales = origin_sales + sku_count
-
-                    sku.stock = new_stock
-                    sku.sales = new_sales
+                    # 库存减少 销量增加
+                    sku.stock -= sku_count
+                    sku.sales += sku_count
                     sku.save()
 
-                    # 累计商品的SPU 销量信息
-                    sku.goods.sales += sku_count
-                    sku.goods.save()
+                    order.total_count += sku_count
+                    order.total_amount += (sku.price*sku_count)
 
-                    # 累计订单基本信息的数据
-                    order.total_count += sku_count  # 累计总金额
-                    order.total_amount += (sku.price * sku_count)  # 累计总额
-
-                    # 保存订单商品
+                    #创建订单商品信息表记录 OrderGoods
                     OrderGoods.objects.create(
-                        order=order,
-                        sku=sku,
-                        count=sku_count,
-                        price=sku.price,
+                        order = order,
+                        sku = sku,
+                        count = sku_count,
+                        price = sku.price,
+
                     )
-
-                # 更新订单的金额数量信息
-                order.total_amount += order.freight
                 order.save()
-
-            except ValidationError:
+            except serializers.ValidationError:
                 raise
             except Exception as e:
                 logger.error(e)
                 transaction.savepoint_rollback(save_id)
                 raise
+            else:
+                transaction.savepoint_commit(save_id)
 
-            # 提交事务
-            transaction.savepoint_commit(save_id)
 
-            # 更新redis中保存的购物车数据
-            pl = redis_conn.pipeline()
-            pl.hdel('cart_%s' % user.id, *cart_selected)
-            pl.srem('cart_selected_%s' % user.id, *cart_selected)
-            pl.execute()
-            return order
+
+        # 删除购物车中已结算的商品
+        pl = redis_conn.pipline()
+        #hash
+        pl.hdel('cart_%s'%user.id,*redis_cart_selected)
+        #set
+        pl.srem('cart_selected_%s'%user.id,*redis_cart_selected)
+        pl.execute()
+
+        # 返回OrderInfo对象
+        return order
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # 获取当前下单用户
+        # user = self.context['request'].user
+        #
+        # # 组织订单编号 20170903153611+user.id
+        # # timezone.now() -> datetime
+        # order_id = timezone.now().strftime('%Y%m%d%H%M%S') + ('%09d' % user.id)
+        #
+        # address = validated_data['address']
+        # pay_method = validated_data['pay_method']
+        #
+        # # 生成订单
+        # with transaction.atomic():
+        #     # 创建一个保存点
+        #     save_id = transaction.savepoint()
+        #
+        #     try:
+        #         # 创建订单信息
+        #         order = OrderInfo.objects.create(
+        #             order_id=order_id,
+        #             user=user,
+        #             address=address,
+        #             total_count=0,  # 每个商品的总数量
+        #             total_amount=Decimal(0),  # 总金额
+        #             freight=Decimal(10),  # 运费
+        #             pay_method=pay_method,
+        #             status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'] if pay_method == OrderInfo.PAY_METHODS_ENUM[
+        #                 'CASH'] else OrderInfo.ORDER_STATUS_ENUM['UNPAID']
+        #         )
+        #         # 获取购物车信息
+        #         redis_conn = get_redis_connection("cart")
+        #         redis_cart = redis_conn.hgetall("cart_%s" % user.id)
+        #         cart_selected = redis_conn.smembers('cart_selected_%s' % user.id)
+        #
+        #         # 将bytes类型转换为int类型
+        #         cart = {}
+        #         for sku_id in cart_selected:
+        #             cart[int(sku_id)] = int(redis_cart[sku_id])
+        #
+        #         # 一次查询出所有商品数据
+        #         skus = SKU.objects.filter(id__in=cart.keys())
+        #
+        #         # 处理订单商品
+        #         for sku in skus:
+        #             sku_count = cart[sku.id]
+        #
+        #             # 判断库存
+        #             origin_stock = sku.stock  # 原始库存
+        #             origin_sales = sku.sales  # 原始销量
+        #
+        #             if sku_count > origin_stock:
+        #                 # 库存不足，就回滚到保存点
+        #                 transaction.savepoint_rollback(save_id)
+        #                 raise serializers.ValidationError('商品库存不足')
+        #
+        #             # 用于演示并发下单
+        #             # import time
+        #             # time.sleep(5)
+        #
+        #             # 减少库存
+        #             new_stock = origin_stock - sku_count
+        #             new_sales = origin_sales + sku_count
+        #
+        #             sku.stock = new_stock
+        #             sku.sales = new_sales
+        #             sku.save()
+        #
+        #             # 累计商品的SPU 销量信息
+        #             sku.goods.sales += sku_count
+        #             sku.goods.save()
+        #
+        #             # 累计订单基本信息的数据
+        #             order.total_count += sku_count  # 累计总金额
+        #             order.total_amount += (sku.price * sku_count)  # 累计总额
+        #
+        #             # 保存订单商品
+        #             OrderGoods.objects.create(
+        #                 order=order,
+        #                 sku=sku,
+        #                 count=sku_count,
+        #                 price=sku.price,
+        #             )
+        #
+        #         # 更新订单的金额数量信息
+        #         order.total_amount += order.freight
+        #         order.save()
+        #
+        #     except ValidationError:
+        #         raise
+        #     except Exception as e:
+        #         logger.error(e)
+        #         transaction.savepoint_rollback(save_id)
+        #         raise
+        #
+        #     # 提交事务
+        #     transaction.savepoint_commit(save_id)
+        #
+        #     # 更新redis中保存的购物车数据
+        #     pl = redis_conn.pipeline()
+        #     pl.hdel('cart_%s' % user.id, *cart_selected)
+        #     pl.srem('cart_selected_%s' % user.id, *cart_selected)
+        #     pl.execute()
+        #     return order
--- meiduo_mall/meiduo_mall/apps/orders/views.py
+++ meiduo_mall/meiduo_mall/apps/orders/views.py
@@ -1,58 +1,59 @@
-from _decimal import Decimal
+from decimal import Decimal
 
 from django.shortcuts import render
 
 # Create your views here.
 from django_redis import get_redis_connection
-from requests import Response
+from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
 from goods.models import SKU
-from orders.serializers import OrderSettlementSerializer,SaveOrderSerializer
+from .serializers import OrderSettlementSerializer,SaveOrderSerializer
 
 
 class OrderSettlementView(APIView):
     """
     订单结算
     """
-    permission_classes = [IsAuthenticated]  # 必须是登录后的用户
-
-    def get(self, request):  # get请求向后端获取订单信息
-        """
-        获取
-        """
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        #获取用户对象 user
         user = request.user
-
-        # 从购物车中获取用户勾选要结算的商品信息
+        #从redis中查询购物车 sku_id selected count
         redis_conn = get_redis_connection('cart')
-        #hash 商品数量 sku_id count
-        redis_cart_dict = redis_conn.hgetall('cart_%s' % user.id)
+
+        #hash  商品数量 sku_id  count
+        redis_cart_dict = redis_conn.hgetall('cart_%s'%user.id)
         #set 勾选商品
-        redis_cart_selected = redis_conn.smembers('cart_selected_%s' % user.id)
+        redis_cart_selected = redis_conn.smembers('cart_selected_%s'%user.id)
 
         cart = {}
+        #cart = {
+        #   勾选商品信息
+        #   sku_id:count
+        # }
         for sku_id in redis_cart_selected:
             cart[int(sku_id)] = int(redis_cart_dict[sku_id])
 
-        # 查询商品信息
+        #查询数据库
         sku_id_list = cart.keys()
         sku_obj_list = SKU.objects.filter(id__in=sku_id_list)
         for sku in sku_obj_list:
             sku.count = cart[sku.id]
-
-        # 运费
+        #运费
         freight = Decimal('10.00')
-
-        serializer = OrderSettlementSerializer({'freight': freight, 'skus': sku_obj_list})
+        #序列化返回
+        serializer = OrderSettlementSerializer({'freight':freight,'skus':sku_obj_list})
         return Response(serializer.data)
 
 class SaveOrderView(CreateAPIView):
     """
     保存订单
     """
-    #接受参数
+    # def post(self):
+    #接受参数 address pay_method
     #校验
     #获取购物车勾选结算的数据
     #创建订单保存
